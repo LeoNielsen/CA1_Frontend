510530216b5ab5fcba83598da7b570075a76551a
LeoNielsen <leo.nielsen21@gmail.com>
2022-03-16 13:38:01 +0100

    added all get functions

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import "./facade"
 import facade from "./facade"
 
 
-
-
 document.querySelector("#searchbutton").addEventListener(`click`, () =>{
   
   document.querySelector("#tbody").innerHTML = "";
@@ -19,36 +17,37 @@ document.querySelector("#searchbutton").addEventListener(`click`, () =>{
        getUserById(input);
       break;
     case "Name":
-      getPersonByName(input);
+      getUserByName(input);
       break;
     case "Phone number":
-      getPersonByPhone(input);
+      getUserByPhone(input);
       break;
-    default:
+    case "Email":
+      getUserByEmail(input);
+      break;
+    case "City":
+      getUserByCity(input);
+      break;
+    case "Zipcode":
+      getUserByZipcode(input);
+      break;
+    case "Street":
+      getUserByStreet(input);
+      break;
+    case "Hobby":
+      getUserByHobby(input);
       break;
   }
 });
 
-
-
 function getUserById(input){
   facade.getUserById(input)
   .then(user => {
-    document.querySelector("#tbody").innerHTML = 
-    `<tr>
-      <td>${user.id}</td>
-      <td>${user.firstname}</td>
-      <td>${user.lastname}</td>
-      <td>${user.email}</td>
-      <td>${user.phones[0].number}</td>
-      <td>${user.address.street} ${user.address.additionalInfo}</td>
-      <td>${user.address.zipcode}</td>
-      <td>${user.address.city}</td>
-    </tr>`
+    tableBuilder(user);
   })
 }
 
-function getPersonByName(input){
+function getUserByName(input){
   facade.getAllUsers()
   .then(users => {
     users.filter(function(user){
@@ -56,42 +55,96 @@ function getPersonByName(input){
         return user;
       }
     }).forEach(user => {
-      let phone;
+      tableBuilder(user)  
+    });
+  })
+}
+
+function getUserByPhone(input){
+  console.log(facade.getUserByPhone(input))
+  facade.getUserByPhone(input)
+  .then(user => {
+    tableBuilder(user);
+  })
+}
+
+function getUserByEmail(input){
+  facade.getAllUsers()
+  .then(users => {
+    users.filter(function(user){
+      if(user.email.toLowerCase().includes(input.toLowerCase())){
+        return user;
+      }
+    }).forEach(user => {
+      tableBuilder(user)  
+    });
+  })
+}
+
+function getUserByCity(input){
+  facade.getAllUsers()
+  .then(users => {
+    users.filter(function(user){
+      if(user.address.city.toLowerCase().includes(input.toLowerCase())){
+        return user;
+      }
+    }).forEach(user => {
+      tableBuilder(user)  
+    });
+  })
+}
+
+function getUserByZipcode(input){
+  facade.getUsersByZipcode(input)
+  .then(users => {
+    users.forEach(user => {
+      tableBuilder(user)
+    })
+  })
+}
+
+function getUserByStreet(input){
+  facade.getAllUsers()
+  .then(users => {
+    users.filter(function(user){
+      const street = `${user.address.street} ${user.address.additionalInfo}`;
+      if(street.toLowerCase().includes(input.toLowerCase())){
+        return user;
+      }
+    }).forEach(user => {
+      tableBuilder(user)  
+    });
+  })
+}
+
+function getUserByHobby(input){
+  facade.getUsersByHobby(input)
+  .then(users => { 
+    users.forEach( user => {
+        tableBuilder(user);
+    })
+  })
+}
+
+
+
+/* Helpers */
+function tableBuilder(user){
+  let phone;
         if (user.phones.length > 0){
           phone = user.phones[0].number
         } else {
           phone = "No Phone";
         }
-
-      document.querySelector("#tbody").innerHTML += 
-      `<tr>
-          <td>${user.id}</td>
-          <td>${user.firstname}</td>
-          <td>${user.lastname}</td>
-          <td>${user.email}</td>
-          <td>${phone}</td>
-          <td>${user.address.street} ${user.address.additionalInfo}</td>
-          <td>${user.address.zipcode}</td>
-          <td>${user.address.city}</td>
-      </tr>`
-    });
-  })
-}
-
-function getPersonByPhone(input){
-  console.log(facade.getUserByPhone(input))
-  facade.getUserByPhone(input)
-  .then(user => {
-    document.querySelector("#tbody").innerHTML = 
-    `<tr>
-        <td>${user.id}</td>
-        <td>${user.firstname}</td>
-        <td>${user.lastname}</td>
-        <td>${user.email}</td>
-        <td>${user.phones[0].number}</td>
-        <td>${user.address.street} ${user.address.additionalInfo}</td>
-        <td>${user.address.zipcode}</td>
-        <td>${user.address.city}</td>
-    </tr>`
-  })
+  document.querySelector("#tbody").innerHTML += 
+  `<tr>
+      <td>${user.id}</td>
+      <td>${user.firstname}</td>
+      <td>${user.lastname}</td>
+      <td>${user.email}</td>
+      <td>${phone}</td>
+      <td>${user.address.street} ${user.address.additionalInfo}</td>
+      <td>${user.address.zipcode}</td>
+      <td>${user.address.city}</td>
+  </tr>`
 }
